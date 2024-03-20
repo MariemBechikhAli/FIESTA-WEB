4664043c30fc049ea89ba00afa52422f8a39cef5
Mariem Bechikh Ali <mariem.bechikhali@grepsys.com>
2024-03-20 16:49:59 +0100

    Authentification component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthentificationComponent } from './authentification/authentification.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AuthentificationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/authentification/authentification.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-authentification',
+  standalone: true,
+  imports: [ButtonModule],
+  templateUrl: './authentification.component.html',
+  styleUrl: './authentification.component.css'
+})
+export class AuthentificationComponent {
+
+}
