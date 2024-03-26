20eac4e3349927a6857a12c03ada98e9c18f74a1
MariemBechikhAli <61082317+MariemBechikhAli@users.noreply.github.com>
2024-03-27 00:36:56 +0100

    login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AuthentificationComponent } from './authentification/authentification.component';
+import { AuthComponent } from './auth/auth.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AuthentificationComponent],
+  imports: [RouterOutlet, AuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { AuthentificationComponent } from './authentification/authentification.component';
+import { AuthComponent } from './auth/auth.component';
 
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch:'full'},
-    {path: 'login', component:AuthentificationComponent}
+    {path: 'login', component:AuthComponent}
 ];
