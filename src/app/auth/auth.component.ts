782c1a427ecab6bc8a82e75decfac02881e9a492
MariemBechikhAli <61082317+MariemBechikhAli@users.noreply.github.com>
2024-04-01 00:38:55 +0100

    Preparation ui OTP

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
-
+  faCoffee = faCoffee;
 }
